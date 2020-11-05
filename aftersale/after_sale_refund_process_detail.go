2dd505cf56e51a025f68e3b457c741b11e575ce5
dengmj968 <848968043@qq.com>
2020-11-05 14:32:53 +0800

    售后单详情

--- aftersale/after_sale_refund_process_detail.go
+++ aftersale/after_sale_refund_process_detail.go
@@ -6,7 +6,7 @@ import (
 )
 
 type ResponseAfterSaleRefundProcessDetail struct {
-	OrderInfo         order.Detail      `mapstructure:"order_info"`
+	OrderInfo         OrderInfo         `mapstructure:"order_info"`
 	Process           ProcessInfo       `mapstructure:"process_info"`
 	AfterSaleInfo     Info              `mapstructure:"aftersale_info"`
 	Logs              []Log             `mapstructure:"logs"`
@@ -46,4 +46,23 @@ type Log struct {
 	Desc       string   `mapstructure:"desc"`        // demo 退货理由：其他
 	Operator   string   `mapstructure:"operator"`    // demo 67939615271
 	Evidence   []string `mapstructure:"evidence"`    // 凭证图片列表
+}
+
+// OrderInfo 子订单信息
+type OrderInfo struct {
+	OrderID     unit.OrderID `mapstructure:"order_id"`     // 订单ID
+	PID         unit.OrderID `mapstructure:"pid"`          // 父订单ID
+	OrderStatus order.SS     `mapstructure:"order_status"` // 订单状态
+	FinalStatus order.SS     `mapstructure:"final_status"` // 子订单状态
+	StatusDesc  string       `mapstructure:"status_desc"`  // 退款状态对应的描述文案
+	CreateTime  string       `mapstructure:"create_time"`  // 订单创建时间
+	ReceiptTime string       `mapstructure:"receipt_time"` // 订单确认收货时间,可能为空字符串
+	ComboNum    uint16       `mapstructure:"combo_num"`    // 下单的sku购买数量
+	ComboAmount unit.Price   `mapstructure:"combo_amount"` // 下单时的sku单价
+	TotalAmount unit.Price   `mapstructure:"total_amount"` // 下单时sku对应的总价
+	PayAmount   unit.Price   `mapstructure:"pay_amount"`   // 下单时改单实际支付的金额(sku总价扣除优惠后的)
+	ShopID      uint64       `mapstructure:"shop_id"`      // 店铺ID
+	ProductID   uint64       `mapstructure:"product_id"`   // 商品id
+	ProductName string       `mapstructure:"product_name"` // 商品名称
+	ProductImg  string       `mapstructure:"product_img"`  // 商品图片
 }
