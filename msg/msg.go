a0743b389d8abd218a1cbaf0655d0a8b002b1e78
dengmj968 <848968043@qq.com>
2020-11-10 16:08:17 +0800

    json解析

--- msg/msg.go
+++ msg/msg.go
@@ -4,13 +4,13 @@ import "github.com/z-vip/doudian_sdk/unit"
 
 //订单创建消息,当买家下单，系统生成订单时，推送此消息
 type TradeCreate struct {
-	Pid         uint64   `mapstructure:"p_id"`         // 父订单ID
-	SIds        []uint64 `mapstructure:"s_ids"`        // 子订单ID列表
-	ShopID      uint64   `mapstructure:"shop_id"`      //  店铺ID
-	CreateTime  uint64   `mapstructure:"create_time"`  //  创建时间
-	OrderStatus uint8    `mapstructure:"order_status"` //父订单状态，订单创建消息的order_status值为"1"
-	OrderType   uint8    `mapstructure:"order_type"`   //订单类型
-	Biz         uint8    `mapstructure:"biz"`          //订单业务类型
+	Pid         uint64   `json:"p_id"`         // 父订单ID
+	SIds        []uint64 `json:"s_ids"`        // 子订单ID列表
+	ShopID      uint64   `json:"shop_id"`      //  店铺ID
+	CreateTime  uint64   `json:"create_time"`  //  创建时间
+	OrderStatus uint8    `json:"order_status"` //父订单状态，订单创建消息的order_status值为"1"
+	OrderType   uint8    `json:"order_type"`   //订单类型
+	Biz         uint8    `json:"biz"`          //订单业务类型
 }
 
 //订单支付/确认消息,
@@ -18,84 +18,84 @@ type TradeCreate struct {
 //在线支付订单，当买家付款成功时
 //货到付款订单，当商家确认订单时（实际并无支付行为）
 type TradePaid struct {
-	Pid         uint64     `mapstructure:"p_id"`         // 父订单ID
-	SIds        []uint64   `mapstructure:"s_ids"`        // 子订单ID列表
-	ShopID      uint64     `mapstructure:"shop_id"`      //  店铺ID
-	OrderStatus uint8      `mapstructure:"order_status"` //父订单状态，订单创建消息的order_status值为"1"
-	OrderType   uint8      `mapstructure:"order_type"`   //订单类型
-	PayType     uint8      `mapstructure:"pay_type"`     //订单支付方式： 0: 货到付款 1: 微信 2: 支付宝
-	PayTime     uint64     `mapstructure:"pay_time"`     //1: 在线订单支付时间 2: 货到付款订单确认时间
-	PayAmount   unit.Price `mapstructure:"pay_amount"`   //订单实付金额
-	Biz         uint8      `mapstructure:"biz"`          //订单业务类型
+	Pid         uint64     `json:"p_id"`         // 父订单ID
+	SIds        []uint64   `json:"s_ids"`        // 子订单ID列表
+	ShopID      uint64     `json:"shop_id"`      //  店铺ID
+	OrderStatus uint8      `json:"order_status"` //父订单状态，订单创建消息的order_status值为"1"
+	OrderType   uint8      `json:"order_type"`   //订单类型
+	PayType     uint8      `json:"pay_type"`     //订单支付方式： 0: 货到付款 1: 微信 2: 支付宝
+	PayTime     uint64     `json:"pay_time"`     //1: 在线订单支付时间 2: 货到付款订单确认时间
+	PayAmount   unit.Price `json:"pay_amount"`   //订单实付金额
+	Biz         uint8      `json:"biz"`          //订单业务类型
 }
 
 //卖家发货消息
 //买家付款后，卖家对所有商品发货完成，且父订单状态为「已发货」时，推送此消息
 type TradeSellerShip struct {
-	Pid          uint64       `mapstructure:"p_id"`          // 父订单ID
-	SIds         []uint64     `mapstructure:"s_ids"`         // 子订单ID列表
-	ShopID       uint64       `mapstructure:"shop_id"`       //  店铺ID
-	OrderStatus  uint8        `mapstructure:"order_status"`  //父订单状态，订单创建消息的order_status值为"1"
-	OrderType    uint8        `mapstructure:"order_type"`    //订单类型
-	UpdateTime   uint64       `mapstructure:"update_time"`   //订单发货时间
-	PayType      uint8        `mapstructure:"pay_type"`      //订单支付方式： 0: 货到付款 1: 微信 2: 支付宝
-	PayTime      uint64       `mapstructure:"pay_time"`      //1: 在线订单支付时间 2: 货到付款订单确认时间
-	PayAmount    unit.Price   `mapstructure:"pay_amount"`    //订单实付金额
-	ReceiverMsg  ReceiverMsg  `mapstructure:"receiver_msg"`  //收货人详细信息
-	LogisticsMsg LogisticsMsg `mapstructure:"logistics_msg"` //发货物流信息
-	Biz          uint8        `mapstructure:"biz"`           //订单业务类型
+	Pid          uint64       `json:"p_id"`          // 父订单ID
+	SIds         []uint64     `json:"s_ids"`         // 子订单ID列表
+	ShopID       uint64       `json:"shop_id"`       //  店铺ID
+	OrderStatus  uint8        `json:"order_status"`  //父订单状态，订单创建消息的order_status值为"1"
+	OrderType    uint8        `json:"order_type"`    //订单类型
+	UpdateTime   uint64       `json:"update_time"`   //订单发货时间
+	PayType      uint8        `json:"pay_type"`      //订单支付方式： 0: 货到付款 1: 微信 2: 支付宝
+	PayTime      uint64       `json:"pay_time"`      //1: 在线订单支付时间 2: 货到付款订单确认时间
+	PayAmount    unit.Price   `json:"pay_amount"`    //订单实付金额
+	ReceiverMsg  ReceiverMsg  `json:"receiver_msg"`  //收货人详细信息
+	LogisticsMsg LogisticsMsg `json:"logistics_msg"` //发货物流信息
+	Biz          uint8        `json:"biz"`           //订单业务类型
 }
 
 //收货人详细信息
 type ReceiverMsg struct {
-	Name string `mapstructure:"name"` //  收货人姓名
-	Tel  string `mapstructure:"tel"`  //  收货人手机号
-	Addr string `mapstructure:"addr"` //  收货地址
+	Name string `json:"name"` //  收货人姓名
+	Tel  string `json:"tel"`  //  收货人手机号
+	Addr string `json:"addr"` //  收货地址
 }
 
 //发货物流信息
 type LogisticsMsg struct {
-	CompanyId     string `mapstructure:"express_company_id"` //  发货快递公司
-	LogisticsCode string `mapstructure:"logistics_code"`     //  发货物流单号
+	CompanyId     string `json:"express_company_id"` //  发货快递公司
+	LogisticsCode string `json:"logistics_code"`     //  发货物流单号
 }
 
 //交易完成消息
 //买家确认收货或系统自动确认收货，且父订单状态变为「已完成」时，推送此消息
 type TradeSuccess struct {
-	Pid          uint64   `mapstructure:"p_id"`          // 父订单ID
-	SIds         []uint64 `mapstructure:"s_ids"`         // 子订单ID列表
-	ShopID       uint64   `mapstructure:"shop_id"`       //  店铺ID
-	OrderStatus  uint8    `mapstructure:"order_status"`  //父订单状态，订单创建消息的order_status值为"1"
-	OrderType    uint8    `mapstructure:"order_type"`    //订单类型
-	CompleteTime uint64   `mapstructure:"complete_time"` //交易完成时间
-	Biz          uint8    `mapstructure:"biz"`           //订单业务类型
+	Pid          uint64   `json:"p_id"`          // 父订单ID
+	SIds         []uint64 `json:"s_ids"`         // 子订单ID列表
+	ShopID       uint64   `json:"shop_id"`       //  店铺ID
+	OrderStatus  uint8    `json:"order_status"`  //父订单状态，订单创建消息的order_status值为"1"
+	OrderType    uint8    `json:"order_type"`    //订单类型
+	CompleteTime uint64   `json:"complete_time"` //交易完成时间
+	Biz          uint8    `json:"biz"`           //订单业务类型
 }
 
 //发货物流变更消息
 //订单已发货，发货物流信息变更时，推送此消息
 type TradeLogisticsChanged struct {
-	Pid          uint64       `mapstructure:"p_id"`          // 父订单ID
-	SIds         []uint64     `mapstructure:"s_ids"`         // 子订单ID列表
-	ShopID       uint64       `mapstructure:"shop_id"`       //  店铺ID
-	OrderStatus  uint8        `mapstructure:"order_status"`  //父订单状态，订单创建消息的order_status值为"1"
-	OrderType    uint8        `mapstructure:"order_type"`    //订单类型
-	UpdateTime   uint64       `mapstructure:"update_time"`   //发货物流变更时间
-	ReceiverMsg  ReceiverMsg  `mapstructure:"receiver_msg"`  //收货人详细信息
-	LogisticsMsg LogisticsMsg `mapstructure:"logistics_msg"` //发货物流信息
-	Biz          uint8        `mapstructure:"biz"`           //订单业务类型
+	Pid          uint64       `json:"p_id"`          // 父订单ID
+	SIds         []uint64     `json:"s_ids"`         // 子订单ID列表
+	ShopID       uint64       `json:"shop_id"`       //  店铺ID
+	OrderStatus  uint8        `json:"order_status"`  //父订单状态，订单创建消息的order_status值为"1"
+	OrderType    uint8        `json:"order_type"`    //订单类型
+	UpdateTime   uint64       `json:"update_time"`   //发货物流变更时间
+	ReceiverMsg  ReceiverMsg  `json:"receiver_msg"`  //收货人详细信息
+	LogisticsMsg LogisticsMsg `json:"logistics_msg"` //发货物流信息
+	Biz          uint8        `json:"biz"`           //订单业务类型
 }
 
 //买家收货信息变更消息
 //当买家收货地址被修改时，推送此消息
 type TradeAddressChanged struct {
-	Pid         uint64      `mapstructure:"p_id"`         // 父订单ID
-	SIds        []uint64    `mapstructure:"s_ids"`        // 子订单ID列表
-	ShopID      uint64      `mapstructure:"shop_id"`      //  店铺ID
-	OrderStatus uint8       `mapstructure:"order_status"` //父订单状态，订单创建消息的order_status值为"1"
-	OrderType   uint8       `mapstructure:"order_type"`   //订单类型
-	UpdateTime  uint64      `mapstructure:"update_time"`  //发货物流变更时间
-	ReceiverMsg ReceiverMsg `mapstructure:"receiver_msg"` //收货人详细信息
-	Biz         uint8       `mapstructure:"biz"`          //订单业务类型
+	Pid         uint64      `json:"p_id"`         // 父订单ID
+	SIds        []uint64    `json:"s_ids"`        // 子订单ID列表
+	ShopID      uint64      `json:"shop_id"`      //  店铺ID
+	OrderStatus uint8       `json:"order_status"` //父订单状态，订单创建消息的order_status值为"1"
+	OrderType   uint8       `json:"order_type"`   //订单类型
+	UpdateTime  uint64      `json:"update_time"`  //发货物流变更时间
+	ReceiverMsg ReceiverMsg `json:"receiver_msg"` //收货人详细信息
+	Biz         uint8       `json:"biz"`          //订单业务类型
 }
 
 /**
@@ -109,14 +109,14 @@ type TradeAddressChanged struct {
 	在线支付订单且订单状态为「备货中」，触发平台风控规则，订单被取消
 */
 type TradeCanceled struct {
-	Pid          uint64   `mapstructure:"p_id"`          // 父订单ID
-	SIds         []uint64 `mapstructure:"s_ids"`         // 子订单ID列表
-	ShopID       uint64   `mapstructure:"shop_id"`       //  店铺ID
-	OrderStatus  uint8    `mapstructure:"order_status"`  //父订单状态，订单创建消息的order_status值为"1"
-	OrderType    uint8    `mapstructure:"order_type"`    //订单类型
-	CancelTime   uint64   `mapstructure:"cancel_time"`   //订单取消时间
-	CancelReason string   `mapstructure:"cancel_reason"` //取消原因
-	Biz          uint8    `mapstructure:"biz"`           //订单业务类型
+	Pid          uint64   `json:"p_id"`          // 父订单ID
+	SIds         []uint64 `json:"s_ids"`         // 子订单ID列表
+	ShopID       uint64   `json:"shop_id"`       //  店铺ID
+	OrderStatus  uint8    `json:"order_status"`  //父订单状态，订单创建消息的order_status值为"1"
+	OrderType    uint8    `json:"order_type"`    //订单类型
+	CancelTime   uint64   `json:"cancel_time"`   //订单取消时间
+	CancelReason string   `json:"cancel_reason"` //取消原因
+	Biz          uint8    `json:"biz"`           //订单业务类型
 }
 
 ////////////////////////////////////////////////////
@@ -125,17 +125,17 @@ type TradeCanceled struct {
 
 //退款消息必定包含的通用信息
 type RefundBase struct {
-	Sid              uint64     `mapstructure:"s_id"`               // 子订单ID
-	Pid              uint64     `mapstructure:"p_id"`               // 父订单ID
-	ShopID           uint64     `mapstructure:"shop_id"`            //  店铺ID
-	AfterSaleId      uint64     `mapstructure:"aftersale_id"`       //  售后单id
-	AfterSaleStatus  uint8      `mapstructure:"aftersale_status"`   //售后状态
-	AfterSaleType    uint8      `mapstructure:"aftersale_type"`     //售后类型
-	RefundAmount     unit.Price `mapstructure:"refund_amount"`      //申请退款的金额（含运费）
-	RefundPostAmount unit.Price `mapstructure:"refund_post_amount"` //申请退的运费金额
-	RefundVoucherNum uint8      `mapstructure:"refund_voucher_num"` //申请退款的卡券的数量
-	ReasonCode       uint8      `mapstructure:"reason_code"`        //申请售后原因码
-	LatestRecord     string     `mapstructure:"latest_record"`      //最近一条操作记录
+	Sid              uint64     `json:"s_id"`               // 子订单ID
+	Pid              uint64     `json:"p_id"`               // 父订单ID
+	ShopID           uint64     `json:"shop_id"`            //  店铺ID
+	AfterSaleId      uint64     `json:"aftersale_id"`       //  售后单id
+	AfterSaleStatus  uint8      `json:"aftersale_status"`   //售后状态
+	AfterSaleType    uint8      `json:"aftersale_type"`     //售后类型
+	RefundAmount     unit.Price `json:"refund_amount"`      //申请退款的金额（含运费）
+	RefundPostAmount unit.Price `json:"refund_post_amount"` //申请退的运费金额
+	RefundVoucherNum uint8      `json:"refund_voucher_num"` //申请退款的卡券的数量
+	ReasonCode       uint8      `json:"reason_code"`        //申请售后原因码
+	LatestRecord     string     `json:"latest_record"`      //最近一条操作记录
 }
 
 /**
@@ -148,7 +148,7 @@ type RefundBase struct {
 
 type RefundTradeCreated struct {
 	RefundBase
-	ApplyTime uint64 `mapstructure:"apply_time"` //售后申请时间
+	ApplyTime uint64 `json:"apply_time"` //售后申请时间
 }
 
 /**
@@ -160,7 +160,7 @@ type RefundTradeCreated struct {
 */
 type RefundAgreed struct {
 	RefundBase
-	AgreeTime uint64 `mapstructure:"agree_time"` //同意退款时间
+	AgreeTime uint64 `json:"agree_time"` //同意退款时间
 }
 
 /**
@@ -169,8 +169,8 @@ type RefundAgreed struct {
 */
 type ReturnApplyAgreed struct {
 	RefundBase
-	Addr      uint8  `mapstructure:"addr"`       //退货地址
-	AgreeTime uint64 `mapstructure:"agree_time"` //同意退款时间
+	Addr      uint8  `json:"addr"`       //退货地址
+	AgreeTime uint64 `json:"agree_time"` //同意退款时间
 }
 
 /**
@@ -179,8 +179,8 @@ type ReturnApplyAgreed struct {
 */
 type BuyerReturnGoods struct {
 	RefundBase
-	Logistics  uint64 `mapstructure:"logistics"`   //退货物流单号
-	ReturnTime uint64 `mapstructure:"return_time"` //退货物流提交时间
+	Logistics  uint64 `json:"logistics"`   //退货物流单号
+	ReturnTime uint64 `json:"return_time"` //退货物流提交时间
 }
 
 /**
@@ -191,7 +191,7 @@ type BuyerReturnGoods struct {
 */
 type RefundRefused struct {
 	RefundBase
-	RefuseTime uint64 `mapstructure:"refuse_time"` //拒绝时间
+	RefuseTime uint64 `json:"refuse_time"` //拒绝时间
 }
 
 /**
@@ -200,7 +200,7 @@ type RefundRefused struct {
 */
 type ReturnApplyRefused struct {
 	RefundBase
-	RefuseTime uint64 `mapstructure:"refuse_time"` //拒绝时间
+	RefuseTime uint64 `json:"refuse_time"` //拒绝时间
 }
 
 /**
@@ -209,7 +209,7 @@ type ReturnApplyRefused struct {
 */
 type RefundSuccess struct {
 	RefundBase
-	SuccessTime uint64 `mapstructure:"success_time"` //退款成功时间¬
+	SuccessTime uint64 `json:"success_time"` //退款成功时间¬
 }
 
 /**
@@ -218,5 +218,5 @@ type RefundSuccess struct {
 */
 type RefundClosed struct {
 	RefundBase
-	CloseTime uint64 `mapstructure:"close_time"` //售后关闭时间
+	CloseTime uint64 `json:"close_time"` //售后关闭时间
 }
