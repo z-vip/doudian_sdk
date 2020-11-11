b0fcc7574c762583aa99f300953f12b246feeeab
dengmj968 <848968043@qq.com>
2020-11-11 15:56:09 +0800

    退货地址

--- aftersale/after_sale_buyer_return.go
+++ aftersale/after_sale_buyer_return.go
@@ -9,4 +9,12 @@ type ArgAfterSaleBuyerReturn struct {
 	Comment   Comm         `paramName:"comment,optional"`    // type = 2 时需要选择拒绝原因
 	Evidence  string       `paramName:"evidence,optional"`   // type = 2 时需要上传图片凭证
 	AddressID string       `paramName:"address_id,optional"` // 商家退货物流收货地址id,不传则使用默认售后收货地址
+
+	//注意：type=1时，可以不入参address_id，直接入参退货地址的详细文本信息,如下：
+	ReceiverName     string `paramName:"receiver_name,optional"`     // 退回商品的收货人姓名
+	ReceiverTel      string `paramName:"receiver_tel,optional"`      // 退回商品的收货手机号
+	ReceiverProvince string `paramName:"receiver_province,optional"` // 退货地址的省（直辖市也必须填，比如北京市）
+	ReceiverCity     string `paramName:"receiver_city,optional"`     // 退货地址的市
+	ReceiverDistrict string `paramName:"receiver_district,optional"` // 退货地址的区
+	ReceiverAddress  string `paramName:"receiver_address,optional"`  // 退货详细地址
 }
